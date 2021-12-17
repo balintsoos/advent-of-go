cccf5bd62dadee52b8122c8ea0316b799da849ee
Balint Soos <soosbalint95@gmail.com>
2021-12-17 23:15:01 +0100

    solve 2021 01 1

--- years/2021/01/1/main.go
+++ years/2021/01/1/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
 	"path"
 	"runtime"
+	"strconv"
 )
 
 func main() {
@@ -14,15 +14,31 @@ func main() {
 	file, err := os.Open(path.Join(path.Dir(filename), "input.txt"))
 
 	if err != nil {
-		log.Fatalf("Failed to open input file.")
+		log.Fatal("Failed to open input file.")
 	}
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-
+	count := 0
+	scanner.Scan()
+	prev := getValue(scanner)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		curr := getValue(scanner)
+		if prev < curr {
+			count += 1
+		}
+		prev = curr
 	}
 
 	file.Close()
+	log.Printf("Count: %v", count)
+}
+
+func getValue(scanner *bufio.Scanner) int {
+	text := scanner.Text()
+	value, err := strconv.Atoi(text)
+	if err != nil {
+		log.Fatalf("Failed to convert string to number: %v", text)
+	}
+	return value
 }
